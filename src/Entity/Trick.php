cec9e9f504c9845724cd0f52f8efebb8641eac32
Mike Agbelou <mike.agbelou@gmail.com>
2026-03-25 18:07:38 +0100

    faet(entities): make entities (Trick, Comment, Image, Video, Group)

---
+++ src/Entity/Trick.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrickRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TrickRepository::class)]
+class Trick
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
