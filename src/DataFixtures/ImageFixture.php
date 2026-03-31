2ea973080f0777f3331332fbc6a7921858b53c95
Mike Agbelou <mike.agbelou@gmail.com>
2026-03-31 23:52:24 +0200

    feat(data-fixtures): make ImageFixture and  add data

---
+++ src/DataFixtures/ImageFixture.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Image;
+use App\Entity\Trick;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class ImageFixture extends Fixture implements DependentFixtureInterface
+{
+    public function getDependencies(): array
+    {
+        return [TrickFixture::class];
+    }
+    
+    public function load(ObjectManager $manager): void
+    {
+        $tricks = ['Indy Grab', 'Backflip', '360', 'Boardslide'];
+
+        foreach ($tricks as $name) {
+
+            $trick = $this->getReference($name, Trick::class);
+
+            for ($i = 1; $i <= 2; $i++) {
+                $image = new Image();
+
+                $image->setUrl(strtolower(str_replace(' ', '', $name)).$i.'.jpg');
+                $image->setAlt($name.' image '.$i);
+                $image->setIsMain($i === 1);
+                $image->setCreatedAt(new \DateTimeImmutable());
+                $image->setTrick($trick);
+
+                $manager->persist($image);
+            }
+        }
+
+        $manager->flush();
+    }
+
+}
