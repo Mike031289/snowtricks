403a318cfb29715f9f1397b22cc26290b28e2459
Adjoukou AGBELOU <64167040+Mike031289@users.noreply.github.com>
2026-04-04 18:45:29 +0200

    Feat/trick crud (#31)
    * feat(trick-crud): make crud templates
    
    * feat(trick-crud): add TrickController Rootes Names,update Trick Form, and config uploads directory in services.yaml
    
    * feat(trick-crud): TrickController: Show a single trick by slug us Root param
    
    * feat(trick-crud): show a single trick with medias and comments
    
    * feat(trick-crud): add trick crud link btn to homepage
    
    * feat(trick-crud): add new trick management: create new trick
    
    * feat(trick-crud): update  trick
    
    * feat(trick-crud)fix:  update  trick
    
    * feat(trick-crud):Update trick system  and his mainImage
    
    feat(trick-crud):fix update, edite Trick and mainImage if needed
    
    * feat(trick-crud): delete trick with CsrfTokenValid

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Image;
 use App\Entity\Trick;
 use App\Form\TrickType;
 use App\Repository\GroupRepository;
@@ -17,6 +16,7 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 #[Route('/trick')]
 final class TrickController extends AbstractController
 {
+    // List all tricks
     #[Route('/liste', name: 'app_trick_index', methods: ['GET'])]
     public function index(TrickRepository $trickRepository): Response
     {
@@ -25,6 +25,7 @@ final class TrickController extends AbstractController
         ]);
     }
 
+    // Create a new trick
     #[Route('/ajouter', name: 'app_trick_new', methods: ['GET', 'POST'])]
     public function new(
         Request $request, 
@@ -34,15 +35,15 @@ final class TrickController extends AbstractController
         ): Response
     {
         $trick = new Trick();
-        $form = $this->createForm(TrickType::class, $trick);
+        $form = $this->createForm(TrickType::class, $trick, [
+            'is_edit' => false, // pass an option to indicate we are in create mode
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
 
             // Author (associate current user as author of the trick)
             $trick->setAuthor($this->getUser());
-            $defaultGroup = $groupRepository->find(1);
-            $trick->setGroups($defaultGroup); // assign to default group (id=1) for now. You can change this logic to allow user to select group in the form.
             
             $trick->setCreatedAt(new \DateTimeImmutable());
             $trick->setUpdatedAt(new \DateTimeImmutable());
@@ -64,79 +65,84 @@ final class TrickController extends AbstractController
                 $trick->setMainImage($newFilename);
             }
 
-            // additional images (collection of file inputs)
-            $images = $form->get('images')->getData();
-
-            foreach ($images as $imageFile) {
-                if ($imageFile) {
-                    $newFilename = uniqid().'.'.$imageFile->guessExtension();
-
-                    $imageFile->move(
-                        $this->getParameter('images_directory'),
-                        $newFilename
-                    );
-
-                    // create a new Image entity and associate it with the Trick
-                    $image = new Image();
-                    $image->setUrl($newFilename);
-                    $image->setTrick($trick);
-
-                    $entityManager->persist($image);
-                }
-            }
-
             $entityManager->persist($trick);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_trick_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('trick/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    // Show a single trick by slug
+    #[Route('/{slug}', name: 'app_trick_show', methods: ['GET'])]
+    public function show(Trick $trick): Response
+    {
+        return $this->render('trick/show.html.twig', [
+            'trick' => $trick
+        ]);
+    }
+
+    // Edit an existing trick
+    #[Route('/{slug}/modifier', name: 'app_trick_edit', methods: ['GET', 'POST'])]
+    public function edit(
+        Request $request,
+        Trick $trick,
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger
+        ): Response 
+    {
+        $form = $this->createForm(TrickType::class, $trick, [
+            'is_edit' => true, // pass an option to indicate we are in edit mode
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $trick->setUpdatedAt(new \DateTimeImmutable());
+
+            // regenerate slug if name changed
+            $slug = $slugger->slug($trick->getName())->lower();
+            $trick->setSlug($slug);
+
+            // handle main image
+            $mainImageFile = $form->get('mainImage')->getData();
+
+            if ($mainImageFile) {
+
+                $newFilename = uniqid($trick->getSlug().'_').'.'.$mainImageFile->guessExtension();
+
+                $mainImageFile->move(
+                    $this->getParameter('images_directory'),
+                    $newFilename
+                );
+
+                $trick->setMainImage($newFilename);
+            }
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('trick/edit.html.twig', [
             'trick' => $trick,
             'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/{slug}', name: 'app_trick_show', methods: ['GET'])]
-    public function show(Trick $trick): Response
-    {
-        if (!$trick) {
-            throw $this->createNotFoundException('Trick not found');
-        }
-        $slug = $trick->getSlug();
-        
-        return $this->render('trick/show.html.twig', [
-            'trick' => $trick,
-            'slug' => $slug
-        ]);
-    }
-
-    #[Route('/{slug}/modifier', name: 'app_trick_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Trick $trick, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(TrickType::class, $trick);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_trick_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('trick/edit.html.twig', [
-            'trick' => $trick,
-            'form' => $form,
-        ]);
-    }
-
+    // Delete a trick
     #[Route('/{slug}/supprimer', name: 'app_trick_delete', methods: ['POST'])]
     public function delete(Request $request, Trick $trick, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$trick->getSlug(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$trick->getSlug(), $request->request->get('_token'))) {
+            
             $entityManager->remove($trick);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_trick_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_home');
     }
 }
--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -7,36 +7,23 @@ use App\Entity\Trick;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TrickType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    {   
+        // Determine if we are in edit mode (if the trick already has an ID) to conditionally set the 'mapped' option for the mainImage field
+        $isEdit = $options['is_edit'] ?? false;
+    
         $builder
             ->add('name')
             ->add('description')
             ->add('mainImage', FileType::class, [
                 'label' => 'Image principale',
-                'mapped' => false, // this field is not associated with any property of the Trick entity and will be handled manually in the controller
-                'required' => true,
-            ])
-
-            // additional images (collection of file inputs)
-            ->add('images', CollectionType::class, [
-                'entry_type' => FileType::class,
-                'entry_options' => [
-                    'label' => false,
-                    'mapped' => false,
-                    'required' => false,
-                ],
-                'allow_add' => true,
-                'allow_delete' => true,
-                'mapped' => false,
-                'required' => false,
-                'by_reference' => false,
+                'mapped' =>  false, // this field is not directly associated with the Trick entity
+                'required' => !$isEdit, // dynamically required if creating a new trick, optional if editing 
             ])
 
             // Group selection (dropdown)
@@ -45,9 +32,6 @@ class TrickType extends AbstractType
                 'choice_label' => 'name', // name is the property to display in the dropdown. Don't use 'id' as it will show the ID instead of the name.
                 'placeholder' => 'Choisir un groupe',
                 'required' => true,
-                'mapped' => true, // this field is associated with the 'groups' property of the Trick entity
-                'multiple' => false, // only one group can be selected
-                'expanded' => false, // display as a dropdown
             ])
         ;
     }
@@ -56,6 +40,7 @@ class TrickType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Trick::class,
+            'is_edit' => false, // default value for the is_edit option
         ]);
     }
 }
