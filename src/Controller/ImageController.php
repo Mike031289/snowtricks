f2c8de4e3c185bdbb0f6365d6e184b80bf0590b2
Mike Agbelou <mike.agbelou@gmail.com>
2026-04-08 19:23:26 +0200

    feat(image-management) : Delete image from Trick Gallery. in ImageController

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ImageController extends AbstractController
 {
-    // #[Route('/image', name: 'app_image')]
-    // public function index(): Response
-    // {
-    //     return $this->render('image/index.html.twig', [
-    //         'controller_name' => 'ImageController',
-    //     ]);
-    // }
+    // This controller is only responsible for deleting images, as they are uploaded and managed in the TrickController.
+    #[Route('/image/{id}/delete', name: 'app_image_delete', methods: ['POST'])]
+        public function delete(Image $image, Request $request, EntityManagerInterface $em): Response
+        {
+            if ($this->isCsrfTokenValid('delete_image_'.$image->getId(), $request->request->get('_token'))) {
+
+                $trick = $image->getTrick();
+
+                // remove file (optional but propre)
+                $filePath = $this->getParameter('images_directory').'/'.$image->getUrl();
+                if (file_exists($filePath)) {
+                    unlink($filePath);
+                }
+
+                $em->remove($image);
+                $em->flush();
+
+                return $this->redirectToRoute('app_trick_edit', [
+                    'slug' => $trick->getSlug()
+                ]);
+            }
+
+            return $this->redirectToRoute('app_home');
+        }
     
 }
