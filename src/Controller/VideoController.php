1255c20865d0cfba698170023b26aef44d6f34ed
Mike Agbelou <mike.agbelou@gmail.com>
2026-04-14 17:03:19 +0200

    feat(video-management): delete videos system and removing url from upload directory : service.yaml config for upload directory

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Video;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class VideoController extends AbstractController
 {
-    #[Route('/video', name: 'app_video')]
-    public function index(): Response
+    #[Route('/video/{id}/delete', name: 'app_video_delete', methods: ['POST'])]
+    public function delete(Video $video, Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('video/index.html.twig', [
-            'controller_name' => 'VideoController',
-        ]);
+        if ($this->isCsrfTokenValid('delete_video_'.$video->getId(), $request->request->get('_token'))) {
+            
+                $trick = $video->getTrick();
+                
+                // remove file (optional but propre)
+                $filePath = $this->getParameter('videos_directory').'/'.$video->getEmbedUrl();
+            
+                if (file_exists($filePath)) {
+                    unlink($filePath);
+                }
+                
+                $em->remove($video); 
+                $em->flush();
+
+                return $this->redirectToRoute('app_trick_edit', [
+                    'slug' => $trick->getSlug()
+                ]);
+        }
+
+        return $this->redirectToRoute('app_home');
+        
     }
 }
