ff57d4cdd3a778451346519f5ee9cf29aca93a51
Mike Agbelou <mike.agbelou@gmail.com>
2026-04-15 19:38:00 +0200

    feat(comment-management): update comment systeme

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CommentController extends AbstractController
 {
-    #[Route('/comment', name: 'app_comment')]
-    public function index(): Response
+    #[Route('/comment/{id}/edit', name: 'app_comment_edit')]
+    public function edit(Comment $comment, Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('comment/index.html.twig', [
-            'controller_name' => 'CommentController',
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        if ($comment->getAuthor() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+             $comment->setCreatedAt(new \DateTimeImmutable());
+             $em->persist($comment);
+             
+            $em->flush();
+
+            return $this->redirectToRoute('app_trick_show', [
+                'slug' => $comment->getTrick()->getSlug()
+            ]);
+        }
+
+        return $this->render('comment/edit.html.twig', [
+            'form' => $form->createView(),
+            'comment' => $comment
         ]);
     }
 }
