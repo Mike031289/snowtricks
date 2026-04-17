363c2287c6639fa9f1da36188b9855d437a979fb
Mike Agbelou <mike.agbelou@gmail.com>
2026-04-17 20:49:24 +0200

    feat(security-voters): make Video voters and implement Voter for DELETE Video and editeTrick template

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -8,10 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
 
 final class VideoController extends AbstractController
 {
     #[Route('/video/{id}/delete', name: 'app_video_delete', methods: ['POST'])]
+    #[IsGranted('VIDEO_DELETE', subject: 'video', message: 'Vous devrez disposer de droits requis', statusCode: 404)]
     public function delete(Video $video, Request $request, EntityManagerInterface $em): Response
     {
         if ($this->isCsrfTokenValid('delete_video_'.$video->getId(), $request->request->get('_token'))) {
---
+++ src/Security/Voter/VideoVoter.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Video;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+final class VideoVoter extends Voter
+{
+public const DELETE = 'VIDEO_DELETE';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::DELETE])
+            && $subject instanceof Video;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        /** @var User $user */
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        if (!$subject instanceof Video) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        return match ($attribute) {
+            self::DELETE => $this->canDelete($subject, $user),
+            default => false
+        };
+
+    }
+    
+    private function canDelete(Video $video, User $user): bool
+    {
+        $trick = $video->getTrick();
+
+        if (!$trick) {
+            return false;
+        }
+
+        // ownership
+        if ($trick->getAuthor() !== $user) {
+            return false;
+        }
+
+        // At lest one image in trick so not delete if only one image in the trick
+        if ($trick->getVideos()->count() <= 1) {
+            return false;
+        }
+
+        return true;
+    }
+}
