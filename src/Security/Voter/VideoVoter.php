935d50750b1bcea8f649618cfda8a667f5c97097
Mike Agbelou <mike.agbelou@gmail.com>
2026-05-04 19:18:53 +0200

    fix(psr-code-quality): update VideoVoter to fix Symfony insight warning

--- src/Security/Voter/VideoVoter.php
+++ src/Security/Voter/VideoVoter.php
@@ -51,7 +51,7 @@ final class VideoVoter extends Voter
         }
 
         // ownership
-        if ($trick->getAuthor() !== $user) {
+        if ($trick->getAuthor()?->getId() === $user->getId()) {
             return false;
         }
 
